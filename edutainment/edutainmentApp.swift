ccf4e05111710147cdd7d90e1c387e06f4227f83
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-10-02 02:15:37 +0200

    Initial Commit

---
+++ edutainment/edutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  edutainmentApp.swift
+//  edutainment
+//
+//  Created by Natalia Nikiforuk on 02/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct edutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
